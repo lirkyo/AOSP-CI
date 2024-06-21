ab238e9db9d35ab2a8282d1d9102228df9cf1aa5
lirkyo <49631026+lirkyo@users.noreply.github.com>
2024-06-22 02:07:33 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,9 +5,8 @@ export PATH=/tmp:$PATH
 export REPO=https://github.com/ProjectSakura/android.git
 export DEVICE=maple_dsds
 export TYPE=userdebug
-export AOSP=Project Sakura
+export AOSP=lineage
 export BRANCH=14
-export FALLBACK=snow
 
 export USE_CCACHE=1
 export CCACHE_SIZE=50G
