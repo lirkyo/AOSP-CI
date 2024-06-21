2e7d5a74cb759c90a48e37c747234dd0909b7846
lirkyo <49631026+lirkyo@users.noreply.github.com>
2024-06-22 02:04:04 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,8 @@ export USE_CCACHE=1
 export CCACHE_SIZE=60G
 export ALLOW_MISSING_DEPENDENCIES=true
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
 # Change to the Source Directry
 cd ~/android
-
 
 # Set-up ccache
 if [ -z "$CCACHE_SIZE" ]; then
@@ -24,41 +15,17 @@ else
     ccache -M ${CCACHE_SIZE}
 fi
 
-echo '[DEBUG]rm -rf  system/sepolicy/prebuilts/api/33.0/private/property.te'
-rm -rf  system/sepolicy/prebuilts/api/33.0/private/property.te
-echo '[DEBUG]cp -r system/sepolicy/private/property.te system/sepolicy/prebuilts/api/33.0/private/'
-cp -r system/sepolicy/private/property.te system/sepolicy/prebuilts/api/33.0/private/
-ls system/sepolicy/prebuilts/api/33.0/private/
 
 # Prepare the Build Environment
 cd ~/android
+
 source build/envsetup.sh
-export ROOMSERVICE_BRANCHES=snow
 
-echo "extracting proprietary"
-mkdir ~/android/system_dump/
-cd ~/android/system_dump/
-git clone -b cust https://github.com/snnbyyds/vendor_oneplus.git ~/vendor_oneplus
-mkdir system/
-mkdir system/vendor/
-mkdir system/odm/
-mkdir system/product/
-mkdir system/system_ext/
-cp -r ~/vendor_oneplus/sdm845-common/proprietary/system .
-cp -r ~/vendor_oneplus/sdm845-common/proprietary/system_ext system/
-cp -r ~/vendor_oneplus/sdm845-common/proprietary/vendor system/
-cp -r ~/vendor_oneplus/sdm845-common/proprietary/product system/
-cp -r ~/vendor_oneplus/fajita/proprietary/odm system/
-cp -r ~/vendor_oneplus/fajita/proprietary/vendor system/
-cd ~/android/device/oneplus/fajita
-./extract-files.sh ~/android/system_dump/
-
-cd ~/android
-lunch evolution_fajita-eng
+lunch lineage_maple_dsds-ap1a-userdebug
 
 # Build!
 echo 'Start Build!'
-mka evolution -j16
+mka bacon -j16
 
 # Exit
 exit 0
